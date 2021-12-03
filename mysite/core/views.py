3cab6954d72c280fd5da0439e98c1ae382c5e7b8
andrecreace <andrecreace@gmail.com>
2021-12-03 17:40:05 -0300

    tttt

--- mysite/core/views.py
+++ mysite/core/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def hello(request, name, age):
     return HttpResponse("<h1>Hello {}, y´re {} years old.</h1>".format(name, age))
+
+# View to addd numbers
+def somar(request, num1, num2):
+    soma = num1 + num2
+    return HttpResponse("A soma entre <b>{}</b> e <b>{}</b> é: <b>{}</b>".format(num1, num2, soma))
